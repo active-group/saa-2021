741ea0a26ec6e42b84efe2769bf2644f51058633
Mike Sperber <sperber@deinprogramm.de>
2021-10-07 11:19:39 +0200

    Update.

--- haskell-code/IntroMike.hs
+++ haskell-code/IntroMike.hs
@@ -2,10 +2,12 @@ module IntroMike where
 
 import Prelude hiding (Functor, Monad, Monoid, Semigroup)
 
--- Tiere auf dem texanischen Highway
+-- Tier auf dem texanischen Highway ist eins der folgenden:
+-- - Gürteltier - ODER -
+-- - Papagei
 
--- Gürteltier:
--- - tot oder lebendig
+-- Gürteltier hat folgende Eigenschaften:
+-- - tot oder lebendig - UND -
 -- - Gewicht
 
 -- discriminated union / enumeration / Aufzählung
@@ -16,7 +18,8 @@ type Weight = Integer
 
 -- Zustand des Tiers zu einem bestimmten Zeitpunkt
 data Animal =
-    Dillo Liveness Weight
+      Dillo Liveness Weight
+    | Parrot String Weight
     deriving Show
 
 dillo1 :: Animal
@@ -27,9 +30,17 @@ dillo2 :: Animal
 -- totes Gürteltier, 11kg
 dillo2 = Dillo Dead 11
 
+-- Begrüßungspapagei, 1kg
+parrot1 :: Animal
+parrot1 = Parrot "Hallo!" 1
+parrot2 :: Animal
+parrot2 = Parrot "Tschüß" 2
+
 -- Tier überfahren
 runOverAnimal :: Animal -> Animal
 runOverAnimal (Dillo l w) = Dillo Dead w
+runOverAnimal (Parrot s w) = Parrot "" w 
+
 
 
 
